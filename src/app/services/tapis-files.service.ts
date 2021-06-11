2aba4d41eb8d052a05069337841c38a7f76900eb
Kumar The Koala <haramkoo@gmail.com>
2021-06-11 14:45:32 -0400

    New UI & Multiple Import

--- src/app/services/tapis-files.service.ts
+++ src/app/services/tapis-files.service.ts
@@ -1,10 +1,8 @@
-import {Injectable, resolveForwardRef} from '@angular/core';
-import {BehaviorSubject, Observable, AsyncSubject, throwError} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
 import { ApiService} from 'ng-tapis';
 import {RemoteFile} from 'ng-tapis';
-import {share, catchError} from 'rxjs/operators';
-import { getValueFromObject } from 'ngx-foundation/typeahead';
-import { CursorError } from '@angular/compiler/src/ml_parser/lexer';
+import {share} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +11,7 @@ export class TapisFilesService {
 
   private baseUrl = 'https://agave.designsafe-ci.org/files/v2/';
   public currentListing: Array<RemoteFile>;
-  public _listing: BehaviorSubject<RemoteFile[]> = new BehaviorSubject<RemoteFile[]>([]);
-  public lester: AsyncSubject<RemoteFile[]> = new AsyncSubject<RemoteFile[]>();
-  public readonly lesting: Observable<RemoteFile[]> = this.lester.asObservable()
+  private _listing: BehaviorSubject<RemoteFile[]> = new BehaviorSubject<RemoteFile[]>([]);
   public readonly listing: Observable<RemoteFile[]> = this._listing.asObservable();
   public readonly IMPORTABLE_TYPES: Array<string> = ['jpg', 'las', 'laz', 'json', 'geojson', 'geotiff', 'tiff', 'gpx'];
 
@@ -23,50 +19,19 @@ export class TapisFilesService {
 
   checkIfSelectable(file: RemoteFile): boolean {
     if (file.type === 'dir') {return false; }
-    //const ext = this.getFileExtension(file);
-    //return this.IMPORTABLE_TYPES.includes(ext);
-    return true
+    const ext = this.getFileExtension(file);
+    return this.IMPORTABLE_TYPES.includes(ext);
   }
 
   private getFileExtension(file: RemoteFile): string {
     return file.name.split('.').pop();
   }
 
+  listFiles(system: string, path: string) {
+    return this.tapis.filesList({systemId: system, filePath: path});
+  }
 
-  listFiles = async (system: string, path: string): Promise<RemoteFile[]> => {
-    let lister: RemoteFile[] = [];
-    return new Promise<RemoteFile[]>((resolve) => {
-      let subscriber1 = this.tapis.filesList({ systemId: system, filePath: path })
-        .subscribe(resp => {
-          const files = resp.result;
-          // This removes the first item in the listing, which in Agave is always a reference to self.
-          const current = files.shift();
-          current.path = this.getParentPath(current.path);
-          current.name = '..';
-          files.unshift(current);
-
-          const newFile = [];
-          files.forEach(function (value, index) {
-          if (value.type == 'file' && (value.path.indexOf('jpg') !== -1) || value.type == 'dir'){
-            newFile.push(value);
-            }
-          })
-
-          this._listing.next(newFile)
-          this.lester.next(newFile)
-            if (this.lester.complete) {
-              // console.log('ready now')
-              lister = this._listing.getValue()
-              subscriber1.unsubscribe()
-              this._listing.complete()
-              resolve(lister)
-            }
-        })
-    })
-  };
-
-
-  private getParentPath(path: string): string {
+  public getParentPath(path: string): string {
     const cleaned = path.replace('//', '/');
     const arr = cleaned.split('/');
     arr.pop();
