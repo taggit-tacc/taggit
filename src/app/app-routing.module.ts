821a5d3c2964ecf55d46345d3d8a130757f56b0e
Ian Park <contact@duckonomy.com>
2022-09-16 12:54:15 -0500

    Smaller fixes and remove comments.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,16 +25,6 @@ export class Activate implements CanActivate {
 
 const routes: Routes = [
   {path: '', component: MainComponent, canActivate: [Activate]
-   // , children: [
-   //  // {path: '', redirectTo: '/tagger', pathMatch: 'full'},
-   //  // TODO IAN cache value on parent route
-   //  // {path: 'gallery', component: ImageGalleryComponent, canActivateChild: [Activate]},
-   //  // {path: 'tagger', component: TaggerComponent, canActivateChild: [Activate]},
-   //  // {path: 'preset', component: PresetGeneratorComponent, canActivateChild: [Activate]}
-
-   //  // {path: 'tagger', component: TagImagesComponent, canActivateChild: [Activate]},
-   //  // {path: 'preset', component: TagGeneratorComponent, canActivateChild: [Activate]}
-   // ]
   },
 
   {path: 'callback', component: CallbackComponent},
