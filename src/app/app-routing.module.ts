c50130ba9090a5c04b83d03693cd907f00d8d65d
Ian Park <contact@duckonomy.com>
2022-09-19 15:35:30 -0500

    Update site.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { NotFoundComponent} from './components/notfound/notfound.component';
 import {AuthService} from './services/authentication.service';
 import {MainComponent} from './components/main/main.component';
 import { CallbackComponent } from './components/callback/callback.component';
+import { TagGeneratorComponent } from './components/side-bar/tag-generator/tag-generator.component';
+import { TagImagesComponent } from './components/side-bar/tag-images/tag-images.component';
 
 @Injectable()
 export class Activate implements CanActivate {
@@ -19,9 +21,10 @@ export class Activate implements CanActivate {
 
 
 const routes: Routes = [
-  {path: '', component: MainComponent, canActivate: [Activate]
-  },
-
+  {path: '', component: MainComponent, canActivate: [Activate], children: [
+	{path: 'tagger', component: TagImagesComponent, canActivateChild: [Activate]},
+	{path: 'preset', component: TagGeneratorComponent, canActivateChild: [Activate]}
+  ]},
   {path: 'callback', component: CallbackComponent},
   {path: '404', component: NotFoundComponent }];
 
