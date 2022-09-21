33c0b6c7f9403178aea72e495fc1d28d0d887a13
Ian Park <contact@duckonomy.com>
2022-09-21 16:46:08 -0500

    Add event emitter output.

--- src/app/components/side-bar/tag-images/form-generator/form-generator.component.ts
+++ src/app/components/side-bar/tag-images/form-generator/form-generator.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { GroupForm } from 'src/app/models/models';
 
@@ -10,7 +10,7 @@ import { GroupForm } from 'src/app/models/models';
 export class FormGeneratorComponent implements OnInit {
   constructor() {}
   @Input() form: GroupForm;
-  newValue = new EventEmitter();
+  @Output() newValue: EventEmitter<any> = new EventEmitter<any>();
   checked = false;
   colorArray: Array<string> = [];
   values = [];
